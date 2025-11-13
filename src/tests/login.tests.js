476499a9a36354ea8b06a3132b5ddc837a86636c
Ivan <Ivan_shopov@outlook.com>
2025-11-13 03:33:54 +0200

    Maybe finall commit sood hi hi

--- src/tests/login.tests.js
+++ src/tests/login.tests.js
@@ -1,8 +1,8 @@
+const { expect } = require('chai');
 const loginPage = require('../po/pages/login.page');
 const basePage = require('../po/pages/base.page');
 const LoginCredentials = require('../po/components/login-components/credentials');
 const log = require('../utils/logger');
-
 
 describe('Login page', () => {
     
@@ -10,26 +10,33 @@ describe('Login page', () => {
         await loginPage.open();
     });
 
-    it('1. Test Login form with empty credentials:', async () => {
+    it('1. Test Login form with empty credentials', async () => {
         log.info('Testing login with empty credentials');
-        await loginPage.setCredentials(LoginCredentials.testUsername,LoginCredentials.testPassword);
+        await loginPage.setCredentials(LoginCredentials.testUsername, LoginCredentials.testPassword);
         await loginPage.clearUsername();
         await loginPage.clearPassword();
         await loginPage.LoginButton();
-        await expect(loginPage.errorMessage).toHaveText('Epic sadface: Username is required');
-    })
-    it('2. Test Login form with credentials by passing Username:', async () => {
+        
+        const errorText = await loginPage.errorMessage.getText();
+        expect(errorText).to.include('Username is required');
+    });
+
+    it('2. Test Login form with credentials by passing Username', async () => {
         log.info('Testing login with only username');
-        await loginPage.setCredentials(LoginCredentials.testUsername,LoginCredentials.testPassword);
+        await loginPage.setCredentials(LoginCredentials.testUsername, LoginCredentials.testPassword);
         await loginPage.clearPassword();
         await loginPage.LoginButton();
-        await expect(loginPage.errorMessage).toHaveText('Epic sadface: Password is required');
-    })
+        
+        const errorText = await loginPage.errorMessage.getText();
+        expect(errorText).to.include('Password is required');
+    });
 
-    it('3. Test Login form with credentials by passing Username & Password:', async () => {
+    it('3. Test Login form with credentials by passing Username & Password', async () => {
         log.info('Testing login with valid username and password');
-        await loginPage.setCredentials(LoginCredentials.validUsername,LoginCredentials.validPassword);
+        await loginPage.setCredentials(LoginCredentials.validUsername, LoginCredentials.validPassword);
         await loginPage.LoginButton();
-        await basePage.TitalText.isDisplayed();
-    })
+        
+        const isDisplayed = await basePage.TitalText.isDisplayed();
+        expect(isDisplayed).to.be.true;
+    });
 });
