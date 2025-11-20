d837709883095e5d2664ac898bc12c0b354efb00
Ivan <Ivan_shopov@outlook.com>
2025-11-20 23:44:39 +0200

    Fix login page

--- src/pageObject/pages/login.page.js
+++ src/pageObject/pages/login.page.js
@@ -1,38 +1,25 @@
 const loginSelectors = require('../components/login-components/login.selectors');
 class LoginPage {
   async setCredentials(username, password) {
-    await this.usernameInput.setValue(username);
-    await this.passwordInput.setValue(password);
+    await $(loginSelectors.usernameInput).setValue(username);
+    await $(loginSelectors.passwordInput).setValue(password);
   }
   async clickLoginButton() {
-    await this.usernameInput.waitForClickable();
-    await this.loginButton.click();
+    await $(loginSelectors.loginButton).waitForClickable();
+    await $(loginSelectors.loginButton).click();
   }
   async clearUsername() {
-    await this.usernameInput.doubleClick();
+    await $(loginSelectors.usernameInput).doubleClick();
     await browser.keys('Backspace');
   }
   async clearPassword() {
-    await this.passwordInput.click();
+    await $(loginSelectors.passwordInput).click();
     await browser.keys(['Control', 'a']);
     await browser.keys('Backspace');
   }
   async getErrorMessage() {
-    await this.errorMessage.waitForDisplayed();
-    return this.errorMessage.getText();
-  }
-  
-  get usernameInput() {
-    return $(loginSelectors.usernameInput);
-  }
-  get passwordInput() {
-    return $(loginSelectors.passwordInput);
-  }
-  get loginButton() {
-    return $(loginSelectors.loginButton);
-  }
-  get errorMessage() {
-    return $(loginSelectors.errorMessage);
+    await $(loginSelectors.errorMessage).waitForDisplayed();
+    return $(loginSelectors.errorMessage).getText();
   }
 }
 
